38bf35a4fde124f244a9f5c532a22ba0630ec83b
Izumrudik32 <mizumrudik@gmail.com>
2025-03-30 11:54:02 +0300

    БЛЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯТЬ ВР

---
+++ Death Penalty/Assets/Scripts/GrabParenter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabParenter : MonoBehaviour
+{
+    public void OnGrab(SelectEnterEventArgs args)
+    {
+        args.interactableObject.transform.SetParent(args.interactorObject.transform);
+    }
+
+    public void UnGrab(SelectExitEventArgs args)
+    {
+        args.interactableObject.transform.SetParent(null);
+    }
+}
+
