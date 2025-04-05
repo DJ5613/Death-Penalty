288fa45b98db5dec0b3683ba9f7d89ef0c75447e
Izumrudik32 <mizumrudik@gmail.com>
2025-04-05 12:47:08 +0300

    УРА РУКИ ЮЛЯТЬБ

---
+++ Death Penalty/Assets/Scripts/HandAnimation1.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+public class HandAnimation1 : MonoBehaviour
+{
+
+    [SerializeField]
+    XRInputValueReader<float> m_TriggerInput;
+    [SerializeField]
+    XRInputValueReader<float> m_GripInput;
+
+    [SerializeField]
+    Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Trigger", m_TriggerInput.ReadValue());
+        animator.SetFloat("Grip", m_GripInput.ReadValue());
+    }
+}
