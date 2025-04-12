6d98a6eecadc009436c592ad68fde4f1a55cacbe
EnenkaBAD <alexandrnazarevech08@gmail.com>
2025-04-12 15:08:35 +0300

    менюшка

---
+++ Death Penalty/Assets/Scripts/Menu/SwitchDifficulty.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchDifficulty : MonoBehaviour
+{
+    [SerializeField] Button _buttonRight;
+    [SerializeField] Button _buttonLeft;
+    [SerializeField] GameObject easyDifficulty;
+    [SerializeField] GameObject mediumDifficulty;
+    [SerializeField] GameObject hardDifficulty;
+    private int dif_num = 1;
+
+    public void NextDifficult(bool left_button, bool right_button)
+    {
+        if (left_button) dif_num--;
+        else dif_num++;
+        if (dif_num > 3) dif_num = 1;
+        if (dif_num < 1) dif_num = 3;        
+        UpdateDifficulty();
+    }
+    private void UpdateDifficulty()
+    {
+        easyDifficulty.SetActive(dif_num == 1);
+        mediumDifficulty.SetActive(dif_num == 2);
+        hardDifficulty.SetActive(dif_num == 3);
+    }
+
+    private void Awake()
+    {
+        _buttonRight.onClick.AddListener(() => NextDifficult(false, true));
+        _buttonLeft.onClick.AddListener(() => NextDifficult(true, false));
+        DontDestroyOnLoad(this.gameObject); // Переносим объект между сценами
+    }
+}
