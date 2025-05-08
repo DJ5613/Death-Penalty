44d8a27830b12d831a85506498670e17541952d9
Izumrudik32 <mizumrudik@gmail.com>
2025-05-08 16:47:22 +0300

    Сложность данной игры заключается в даунах на разрабах

--- Death Penalty/Assets/Scripts/State Machine/BossStateManager.cs
+++ Death Penalty/Assets/Scripts/State Machine/BossStateManager.cs
@@ -36,6 +36,21 @@ public class BossStateManager : MonoBehaviour
 
     private void Start()
     {
+        switch (SwitchDifficulty.dif_num)
+        {
+            case 1:
+                wolkSpeed = (float)(wolkSpeed * 0.7);
+                enemyHP = (float)(enemyHP * 0.5);
+                enemyDamage = (float)(enemyDamage * 0.5);
+                break;
+            case 2:
+                break;
+            case 3:
+                wolkSpeed = (float)(wolkSpeed * 1.2);
+                enemyHP = (float)(enemyHP * 1.5);
+                enemyDamage = (float)(enemyDamage * 1.5);
+                break;
+        }
         if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
         _cachedPath = new NavMeshPath();
         SwichState(idleState);
