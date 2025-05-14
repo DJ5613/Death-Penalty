25f35dc6919ff001c92ec8252925c970c15f4a24
EnenkaBAD <alexandrnazarevech08@gmail.com>
2025-05-14 23:05:12 +0300

    БОСС МОЖЕТ УМЕРЕТЬ!!! Я ЩАС ЗАПЛАЧУ ОТ СЧАСТЬЯ!!!

--- Death Penalty/Assets/Scripts/Hits/Damager.cs
+++ Death Penalty/Assets/Scripts/Hits/Damager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Damager : MonoBehaviour
 {
     [SerializeField] EnemyStateManager manager;
+    [SerializeField] BossStateManager bossManager;
     public void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<DamageDetector>(out DamageDetector detector)) //урон по игроку
         {
-            detector.OnDamageDetected(manager.enemyDamage);
+            if (manager!= null) detector.OnDamageDetected(manager.enemyDamage);
+            else detector.OnDamageDetected(bossManager.enemyDamage);
         }
     }
 }
